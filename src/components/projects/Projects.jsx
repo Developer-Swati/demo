d7b853c101f01ae688ea637a3d2509107ee88a1b
Swati <125959657+Developer-Swati@users.noreply.github.com>
2024-03-22 11:37:45 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import './projects.css'
-// import IMG1 from '../../assets/img5.jpg'
-// import IMG2 from '../../assets/img2.png'
-// import IMG3 from '../../assets/img3.png'
-// import IMG4 from '../../assets/img4.png'
+
 
 const Projects = () => {
   return (
@@ -33,8 +30,8 @@ const Projects = () => {
             <h3>My Portfolio(This website)</h3>
             <small className='text-light'>ReactJs</small>
             <div className="portfolio__item-cta">
-              <a href="https://github.com/Developer-Swati/My-Portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>
-              <a href="https://github.com/Developer-Swati/My-Portfolio" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+              <a href="https://github.com/Developer-Swati/demo" target="_blank" rel='noreferrer' className='btn'>Github</a>
+              <a href="https://developer-swati.github.io/demo/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
             </div>
             
         </article>
